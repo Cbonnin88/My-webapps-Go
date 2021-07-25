8cda315481ef8071b8bbc659ca570bd06fe37fd6
Chris <chrisbit.carrington@gmail.com>
2021-07-25 15:54:29 +0200

    Updated Go Webapp

--- Go-Webapp/goWebapp.go
+++ Go-Webapp/goWebapp.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Welcome to my first Go Web app")
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
